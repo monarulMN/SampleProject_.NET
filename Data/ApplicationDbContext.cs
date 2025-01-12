1ff7812269e147858d6744872ecc485b3db1743a
monarulMN <mdmonarulislam720@gmail.com>
2025-01-12 23:20:49 +0600

    First Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FirstWebApp.Models;
+
+namespace FirstWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<FirstWebApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
